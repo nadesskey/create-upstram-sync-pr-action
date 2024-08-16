766d35ce394936b13d90fa9667b29c7159325f29
tai-cha <dev@taichan.site>
2024-08-16 18:10:38 +0900

    almost all

---
+++ src/utils/git.ts
@@ -0,0 +1,71 @@
+import { exec } from '@actions/exec';
+import { logger } from './config';
+
+/* *
+ * GitHubのowner名やrepo名が正しくない場合はエラーにする
+  * @param value GitHubのowner名やrepo名
+  * @param options オプション
+  * @param options.name GitHubのowner名やrepo名の名前
+  * @param options.throwOnFail エラーが発生した場合に例外をスローするかどうか
+  * @returns 正しい場合はtrue、正しくない場合はfalse
+  */
+const validateGitHub = (value: string, options?: { name?: string, throwOnFail?: boolean }) => {
+  const pattern = /^[a-zA-Z0-9-]+$/;
+  const result = pattern.test(value);
+
+  if (!result) {
+    let msg = ''
+    if (options?.name) {
+      msg = `Invalid GitHub ${options.name} name: ${value}`;
+    } else {
+      msg = `Invalid GitHub name: ${value}`;
+    }
+
+    if (options?.throwOnFail) { throw new Error(msg); }
+    console.debug(msg);
+
+    throw new Error(msg);
+  }
+
+  return result;
+}
+
+export async function initializeGitConfig() {
+  await exec('git', ['config', '--global', 'user.name', 'github-actions']);
+  await exec('git', ['config', '--global', 'user.email', 'actions@github.com']);
+}
+
+export async function initializeUpstream(token: string, owner?: string, repo?: string) {
+  if (owner == null || repo == null) {
+    throw new Error("Upstream owner and repo must be specified or provided through a fork relationship.");
+  }
+
+  validateGitHub(owner, { name: 'owner', throwOnFail: true });
+  validateGitHub(repo, { name: 'repo', throwOnFail: true });
+
+  const upstreamUrl = `https://github.com/${owner}/${repo}.git`;
+
+  // 既にupstreamが存在する場合を考慮して、一度削除してから追加することで上書きする
+  await exec('git', ['remote', 'remove', 'upstream'], { ignoreReturnCode: true });
+  await exec('git', ['remote', 'add', 'upstream', upstreamUrl]);
+
+  // (private repoなこともあるため)tokenを使って認証する
+  await exec('git', ['config', '--global', `url."https://x-access-token:${token}@github.com".insteadOf`, 'https://github.com']);
+
+  // upstreamをfetchする
+  // upstreamが取得できない場合はエラーにする
+  try {
+    await exec('git', ['fetch', 'upstream']);
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(error.message);
+    }
+    throw new Error(`Failed to fetch upstream repository: ${upstreamUrl}`);
+  }
+
+  return {
+    owner,
+    repo,
+    upstreamUrl,
+  };
+}
---
+++ src/utils/logger.ts
@@ -0,0 +1,37 @@
+export default class Logger {
+  static instances: Logger[];
+  #prefix: string;
+
+  constructor(prefix = '') {
+    this.#prefix = prefix;
+  }
+
+  log(...args: any[]) {
+    console.log(this.#prefix, ...args);
+  }
+
+  debug(...args: any[]) {
+    console.debug(this.#prefix, ...args);
+  }
+
+  info(...args: any[]) {
+    console.info(this.#prefix, ...args);
+  }
+
+  warn(...args: any[]) {
+    console.warn(this.#prefix, ...args);
+  }
+
+  error(...args: any[]) {
+    console.error(this.#prefix, ...args);
+  }
+
+  static use(prefix: string) {
+    let instance = Logger.instances.find(i => i.#prefix === prefix);
+    if (!instance) {
+      instance = new Logger(prefix);
+      Logger.instances.push(instance);
+    }
+    return instance;
+  }
+}
